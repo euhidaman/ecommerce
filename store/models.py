25018355a2653ce144350e7aab691b7999c68d31
Euhid Aman <aman.derax20@gmail.com>
2020-11-28 23:35:30 +0530

    Added image field to Products model

--- store/models.py
+++ store/models.py
@@ -14,6 +14,7 @@ class Product(models.Model):
 	name = models.CharField(max_length=200)
 	price = models.FloatField()
 	digital = models.BooleanField(default=False,null=True, blank=True)
+	image = models.ImageField(null=True, blank=True)
 
 	def __str__(self):
 		return self.name
--- store/views.py
+++ store/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from .models import  *
 
 def store(request):
-     context = {}
-     return render(request, 'store/store.html', context)
+    products = Product.objects.all()
+    context = {"products":products}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
-     context = {}
-     return render(request, 'store/cart.html', context)
+    context = {}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
-      context = {}
-      return render(request, 'store/checkout.html', context)
+    context = {}
+    return render(request, 'store/checkout.html', context)
