7b044568881fcc7adcff68e1a5838b5289865b16
Euhid Aman <aman.derax20@gmail.com>
2020-11-29 12:08:58 +0530

    Rendered order summary in checkout page

--- store/views.py
+++ store/views.py
@@ -22,7 +22,17 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html', context)
+    # giving the same code as cart, bcz same, total data will be rendered in frontend
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        # get_or_create is used to search for a qiven object,
+        # and, if it isn't there, it then creates tht model object
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        # the below line, is used to query from the OrderItem model
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
 
-# Render cart data
+    context = {'items':items, 'order':order}
+    return render(request, 'store/checkout.html', context)
