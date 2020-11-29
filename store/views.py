61d208179d24f7ff85d2821f36ebffd3796ec78e
Euhid Aman <aman.derax20@gmail.com>
2020-11-29 11:34:42 +0530

    Created and queryed values in checkout page frm backend

--- store/views.py
+++ store/views.py
@@ -7,9 +7,20 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        # get_or_create is used to search for a qiven object,
+        # and, if it isn't there, it then creates tht model object
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        # the below line, is used to query from the OrderItem model
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
+
+# Render cart data
