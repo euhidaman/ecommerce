36c87f5aa2bcfb0d12b5970bc1dcfaa0d51fff1a
Euhid Aman <aman.derax20@gmail.com>
2020-11-30 20:59:08 +0530

    wrote code to increment items, and show changes in cart

--- store/views.py
+++ store/views.py
@@ -44,9 +44,31 @@ def checkout(request):
 
 
 def updateItem(request):
-    data = json.loads(request.data)
+    data = json.loads(request.body)
     productId = data['productId']
     action = data['action']
     print('Action :', action)
     print('productId :',productId)
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    # below line attaches the order to the given customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    # in the below line, we r using 'get_or_create' to change the values of orderItem, if it already exists
+    # so, if it already exists, we don't wan't to create orderItem again, we just want to change the quantity of orderItem
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action =='add':
+        # by clicking up arrow, increment orderItem by 1
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        # by clicking up arrow, decrement orderItem by 1
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    # save quantity of products, for an order
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        # remove the orderItem from cart, when quantity reaches 0, or below it
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
